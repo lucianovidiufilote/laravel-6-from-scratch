5869fd8bf1fbec17544d040a631b839e66978de5
Lucian Filote <lucianovidiu.filote@gameloft.com>
2020-01-20 13:46:19 +0200

    Section 3 done

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,23 +12,9 @@ class PostsController extends Controller
 {
     public function show($slug)
     {
-//        $posts = [
-//            'first' => 'HEllo!',
-//            'second' => 'Better'
-//        ];
-
-        //$post = DB::table('posts')->where('slug', $slug)->first();
-        $post = Post::where('slug', $slug)->firstOrFails();
-
-//        dd($post);
-
-//        if (!array_key_exists($post, $posts)) {
-//            abort(404, 'Sorry, post not found!');
-//        }
-
         return view('post',
             [
-                'post' => $post
+                'post' => Post::where('slug', $slug)->firstOrFails()
             ]);
     }
 }
